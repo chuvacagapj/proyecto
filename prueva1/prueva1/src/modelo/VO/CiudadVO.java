f4f5d7e6cfbd602595241a2ff97abaf14393641f
chuvacagapj@hotmail.com <Jesus Garcia>
2014-08-17 18:07:30 -0600

    Se desarrolla la clase VO con getters y setters simples, aparte de sus atributos, aparte de que se altera el archivo .gitignore

--- prueva1/prueva1/src/modelo/VO/CiudadVO.java
+++ prueva1/prueva1/src/modelo/VO/CiudadVO.java
@@ -1,5 +1,42 @@
-package VO;
+package modelo.VO;
+
+/* esta clase es un Value Object por lo que su unica funcion es el paso de informacion por bloque entre
+ * todas y cada una de las partes que necesiten de la informacion.
+ * */
 
 public class CiudadVO {
 
+	// atributos
+
+	private Integer clave;
+	private String nombre;
+	private Integer habitantes;
+
+	// getters
+
+	public Integer getClave() {
+		return this.clave;
+	}
+
+	public String getNombre() {
+		return this.nombre;
+	}
+
+	public Integer getHabitantes() {
+		return this.habitantes;
+	}
+
+	// setters
+
+	public void setClave(Integer clave) {
+		this.clave = clave;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public void setHabitantes(Integer habitantes) {
+		this.habitantes = habitantes;
+	}
 }
